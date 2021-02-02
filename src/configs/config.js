c2a9ecbb2c2e95772f7a92956454ce1e992d0730
Sanjit Singh <sanjit.singh@uidai.net.in>
2021-02-02 17:56:58 +0530

    Base URL changed to attendance-ss

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,10 +1,11 @@
 import { Platform } from 'react-native'
 const configs = {
-    BASE_URL: 'https://www.schoolattendancegujarat.org/api',
+    // BASE_URL: 'https://www.schoolattendancegujarat.org/api',
+    BASE_URL: 'https://attendance-ss.gujarat.gov.in/api',
 }
 
 export default configs;
 
-export const apkVersion =  "2.1.2"
-export const apkVersionId = Platform.OS === "ios" ? "0" : "13"
+export const apkVersion =  "2.1.3"
+export const apkVersionId = Platform.OS === "ios" ? "0" : "14"
 export const apkURL = Platform.OS == 'ios' ? 'itms-apps://itunes.apple.com/lookup?bundleId=com.hwrecognisation' : 'http://play.google.com/store/apps/details?id=com.hwrecognisation'
