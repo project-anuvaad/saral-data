780ace92606f7a75f982b81a84cb7baecf1817bd
Sanjit Singh <sanjit.singh@uidai.net.in>
2021-02-12 11:26:13 +0530

    Updated apk version and name for production release

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,11 +1,11 @@
 import { Platform } from 'react-native'
 const configs = {
-    // BASE_URL: 'https://www.schoolattendancegujarat.org/api',
-    BASE_URL: 'https://attendance-ss.gujarat.gov.in/api',
+    BASE_URL: 'https://www.schoolattendancegujarat.org/api',
+    // BASE_URL: 'https://attendance-ss.gujarat.gov.in/api',
 }
 
 export default configs;
 
-export const apkVersion =  "2.1.4"
-export const apkVersionId = Platform.OS === "ios" ? "0" : "15"
+export const apkVersion =  "2.1.3"
+export const apkVersionId = Platform.OS === "ios" ? "0" : "14"
 export const apkURL = Platform.OS == 'ios' ? 'itms-apps://itunes.apple.com/lookup?bundleId=com.hwrecognisation' : 'http://play.google.com/store/apps/details?id=com.hwrecognisation'
