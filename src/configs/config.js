62cc5560a042e17d6e8f3e4b864892321e1349cd
arman ansari <arman.ansari@tarento.com>
2021-08-01 20:26:08 +0530

    update version in build.gradle and config.js

--- src/configs/config.js
+++ src/configs/config.js
@@ -6,6 +6,6 @@ const configs = {
 
 export default configs;
 
-export const apkVersion = "3.0.0"
-export const apkVersionId = Platform.OS === "ios" ? "0" : "15"
+export const apkVersion = "3.0.1"
+export const apkVersionId = Platform.OS === "ios" ? "0" : "16"
 export const apkURL = Platform.OS == 'ios' ? 'itms-apps://itunes.apple.com/lookup?bundleId=com.hwrecognisation' : 'http://play.google.com/store/apps/details?id=com.hwrecognisation'
